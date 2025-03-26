ba97ea73e2f138490741f55575d1ce98bdd5b8ea
Sharv Utpat <sutpat1@gmail.com>
2025-03-26 18:59:51 -0500

    Initial commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,65 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+
+import { TypeAnimation } from 'react-type-animation';
+
+
+
+const HeroSection= () => {
+    return (
+        <section> 
+
+        <div className = "grid grid-cols-1 lg:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+                <h1 className="text-white mb-4 text-4xl lg:text-6xl
+                font-extrabold"> 
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I am {""}
+                </span>
+                <br></br>
+                <TypeAnimation
+            sequence={[
+            // Same substring at the start will only be typed out once, initially
+            'Sharv Utpat',
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            'a Graduate from UT Dallas',
+            1000,
+            'a Full Stack Software Developer',
+            1000,
+            'Interested in AI/ML',
+            1000,
+      ]}
+      wrapper="span"
+      speed={50}
+      repeat={Infinity}
+    />
+                 </h1>
+                
+                <p className="text-[#ADB7BE] text-base text-lg mb-6 lg:text-xl ">
+                    I am a recent graduate from the University of Texas at Dallas.
+                </p>
+                <div className="mt-6">
+                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-dark-blue-500 via-indigo-500 to-pink-500 hover:bg-slate-200 text-white border border-white mt-3">
+                        Hire Me
+                    </button>
+                    <button  className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-dark-blue-500 via-indigo-500 to-pink-500 hover:bg-slate-800 text-white  mt-3">
+                      <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>  
+                    </button>
+                </div>
+            </div>
+            <div className="col-span-5 flex justify-center items-center">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg: w-[400px] lg: h-[400px] relative flex justify-center items-center">
+                <Image 
+                src="/images/SharvWhiteBackground.png" 
+                alt="hero image" 
+                className="rounded-full"
+                width={300} 
+                height={300} // Required for Next.js Image component
+                />
+                </div>
+            </div>
+        </div>
+        </section>
+    )
+}
+export default HeroSection;
