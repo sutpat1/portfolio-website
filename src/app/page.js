2e5031bda9e008267953cc73129b030c68bf1d3c
Sharv Utpat <sutpat1@gmail.com>
2025-03-27 16:52:04 -0500

    Added Navigation Bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection"
+import NavBar from "./components/NavBar"
 
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className = "container mx-auto px-12 py-4">
+        <NavBar/>
+
+      <div className = "container mx-auto px-12 py-4 mt-16">
         <HeroSection/>
       </div>
        
