d7c396b022e8b796e35a0455364f4e617546acb4
Sharv Utpat <sutpat1@gmail.com>
2025-04-02 22:17:43 -0500

    Added project descriptions and CV

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -41,7 +41,9 @@ const HeroSection= () => {
                     <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-orange-500 via-red-500 to-yellow-500 hover:bg-slate-200 text-white border border-white mt-3">
                         Hire Me
                     </button>
-                    <button  className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-orange-500 via-yellow-500 to-red-500 hover:bg-slate-800 text-white  mt-3">
+                    <button  
+                     onClick={() => window.open("/documents/March2025CVSharvUtpat.pdf", "_blank")}
+                    className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-orange-500 via-yellow-500 to-red-500 hover:bg-slate-800 text-white  mt-3">
                       <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>  
                     </button>
                 </div>
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -6,8 +6,8 @@ import ProjectTag from './ProjectTag'
 const projectsData = [
   {
     id: 1,
-    title: "React Portfolio Website",
-    description: "Project 1 description",
+    title: "Portfolio Website",
+    description: "For this project, I used React, Next.js, and JavaScript to build a personal portfolio website. By leveraging React for dynamic content rendering and Next.js for server-side rendering and optimization, I created a fast, responsive, and SEO-friendly website. I focused on building a clean, user-friendly interface to showcase my projects, skills, and experience, ensuring seamless navigation and a polished user experience.",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/sutpat1/my-portfolio",
@@ -16,7 +16,7 @@ const projectsData = [
   {
     id: 2,
     title: "Fast Food AI Chatbot Application",
-    description: "Project 2 description",
+    description: "I led a 4-member team to develop a full-stack AI-powered fast food chatbot for In-N-Out using Flask and spaCy, managing project planning and team coordination via Jira and Confluence. We used a Kanban board to ensure timely feature delivery. I applied ML-based NLP techniques with spaCy to process customer inquiries, enabling understanding of menu items, prices, and orders. I also built a dynamic menu system from a CSV file and designed a user-friendly web interface for seamless front-end and back-end integration.",
     image: "/images/projects/2.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/sutpat1/CapstoneChatBot",
@@ -25,7 +25,7 @@ const projectsData = [
   {
     id: 3,
     title: "Growin' Buds Application",
-    description: "Project 3 description",
+    description: "I collaborated with a team of four developers to design and implement the user interface for a gardening application. Using JavaScript and React Native, we built a dynamic, cross-platform mobile app that ensured a responsive and intuitive user experience. My focus was on frontend development, UI/UX design, and ensuring seamless integration across devices. We also uploaded the app to Expo, enabling easy deployment, testing, and distribution for both iOS and Android platforms.",
     image: "/images/projects/3.png",
     tag: ["All", "Mobile"],
     gitUrl: "https://github.com/sutpat1/Growin-Buds",
@@ -34,7 +34,7 @@ const projectsData = [
   {
     id: 4,
     title: "Connect 4 Application",
-    description: "Project 4 description",
+    description: "I collaborated with a team of three peers to develop a Connect Four game in MIPS assembly, deepening my knowledge of software and computer architecture. I gained experience with low-level instructions, memory management, and processor operations while implementing a UI/UX-focused graphical interface with bitmap graphics, optimizing software for graphics rendering within MIPS constraints.",
     image: "/images/projects/4.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/sutpat1/Connect4Project",
