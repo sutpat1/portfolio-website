2890e0c9d0cb0fef5dc69f8b02bf20133c97d96f
Sharv Utpat <sutpat1@gmail.com>
2025-04-10 17:04:23 -0500

    Removed logo words

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -28,7 +28,7 @@ const NavBar = () => {
             <div className="flex flex-wrap items-center justify-between mx-auto p-8">
 
                 <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
-                    LOGO
+                    
                     </Link>
                 
                 <div className="mobile-menu block md:hidden">
