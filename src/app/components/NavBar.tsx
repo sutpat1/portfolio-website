566fd4c0b9c4b7c8445bdc079b4ee03da9481557
Sharv Utpat <sutpat1@gmail.com>
2025-04-15 20:16:57 -0500

    Fixed navbar mobile

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -35,22 +35,22 @@ const NavBar: React.FC = () => {
   
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100 border-b border-gray-800">
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-3 md:p-8">
+        <Link href={"/"} className="text-xl md:text-5xl text-white font-semibold">
         </Link>
         
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
-            <button 
+            <button
               onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-2 py-1 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
               <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
-            <button 
+            <button
               onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-2 py-1 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
               <XMarkIcon className="h-5 w-5" />
             </button>
